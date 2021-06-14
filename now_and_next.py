82341827746cb164d4c548017af0f366029e2f90
Steve Carter <steve.carter@etas.com>
2021-06-14 11:58:09 +0100

    put in get_cursor from prototype

--- now_and_next.py
+++ now_and_next.py
@@ -3,12 +3,27 @@ TODO
 
 Set a minimum size or stop the clockface from breaking when you go too small
 """
+import datetime
 
 import tkinter as TK
 
 CLOCK_FACE_COLOR='white'
 ARC_COLOR='pink'
 CLOCK_PADDING=10
+
+## Time Stuff
+
+DEBUG_TIME_OFFSET=datetime.timedelta(seconds=3600)
+#DEBUG_TIME_OFFSET=datetime.timedelta(hours=-10,seconds=900)
+def get_cursor():
+    """ Return the time that we are interested in """
+    cursor=datetime.datetime.now(datetime.timezone.utc) + DEBUG_TIME_OFFSET
+    # log what time offset caused 
+    return cursor
+
+## Calendar/Outlook stuff
+
+## UI stuffs
 
 def biggest_square(x1,y1,x2,y2):
     """ return x,y,x2,y2 for a square centered in the given 
@@ -73,9 +88,6 @@ class NowAndNextUI(TK.Frame):
         TK.Frame.__init__(self,master,padx=20, pady=15)
         self.pack(expand=TK.YES, fill=TK.BOTH)
 
-        self.master.title('Hello World')
-        self.master.tk_setPalette(background='#e6e6e6')
-
         left_frame = TK.Frame(self, width=140, height=140)
         self.clock_face=TimerWidget(left_frame, width=140,height=140).pack(side=TK.TOP)
         left_frame.pack(side=TK.LEFT, fill=TK.BOTH)
@@ -84,6 +96,28 @@ class NowAndNextUI(TK.Frame):
         self.next_label = TK.Label(right_frame, text='Awaiting data...',justify=TK.LEFT)
         self.next_label.pack(side=TK.LEFT, expand=TK.YES, fill=TK.BOTH)
         right_frame.pack(side=TK.RIGHT, expand=TK.YES, fill=TK.BOTH)
+
+    def refresh_canvas(self):
+        self.after(1000, self.refresh_canvas)
+        time_now = get_cursor()
+
+        if time_now.minute != self.previous_minute:
+            ongoing, upcoming = refresh_database(time_now)
+            self.next_deadline = upcoming.start
+            self.previous_minute = time_now.minute
+            lines = [time_now.strftime('%c')]
+            lines.extend(map( lambda ev: f'    {ev[0].subject}', ongoing )) 
+            lines.append(f'Next:\n    {upcoming.subject}')
+            
+            self.next_label.config(text='\n'.join(lines))
+
+        self.clock_face.set_time( self.next_deadline - time_now )
+
+
+    def mainloop(self):
+        self.previous_minute = None
+        self.after(1000, self.refresh_canvas)
+        self.master.mainloop()
 
 
 if __name__ == "__main__":
